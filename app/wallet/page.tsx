193cfec02ae3ec9e5828f6636cc3996b9aea8cd2
Punyawish Patumhirunruksa <87409329+Punpunyawish@users.noreply.github.com>
2023-10-21 19:03:57 +0700

    create wallet page

---
+++ app/wallet/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Payhistory() {
+    return(
+        <>
+        <h1> Pay_history </h1>
+        <p> transfer to B: 300 bulb </p>
+        </>
+    )
+}

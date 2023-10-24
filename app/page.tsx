7cc7134eaaca32c54134a5d39076a27612eef813
Akukinaqua <guynachanok2@gmail.com>
2023-10-25 00:42:46 +0700

    delete test pic from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Header from "@/components/Home/Header";
 import ProductCard from "@/components/Home/ProductCard";
-import ImagePlacer from "@/components/Kcomponent/ImagePlacer";
+
 export default function Home() {
   return (
     <>
       <Header />
       <ProductCard />
-      <ImagePlacer />
     </>
   );
 }

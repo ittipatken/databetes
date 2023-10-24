35faefda88160e10084f213e35d20f769b4c923c
BBenneddyy <kanazaa2@gmail.com>
2023-10-24 17:10:12 +0700

    try to place img in tester app

---
+++ app/homepage/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Home/Header";
+import ProductCard from "@/components/Home/ProductCard";
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <ProductCard />
+    </>
+  );
+}
---
+++ app/tester/page.tsx
@@ -0,0 +1,30 @@
+/*import React from "react";
+import img1 from './images/imgtest.jpg';
+
+function Addimage() {
+  return (
+   
+    <div>
+      eslint-disable-next-line @next/next/no-img-element
+      <img src={img1} alt="" />
+    </div>
+  );
+}
+export default Addimage;*/
+import React from 'react'
+import Image from 'next/image'
+ 
+function Addimage() {
+  return (
+    <div>
+    <Image
+      src="/next/images/imgtest.jpg"
+      alt=""
+      width={1841}
+      height={933}
+    />
+    </div>
+  )
+}
+ 
+export default Addimage

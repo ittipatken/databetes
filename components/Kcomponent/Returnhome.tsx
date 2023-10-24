733e6cbbb461231108bbd1949081c0f86431c454
BBenneddyy <kanazaa2@gmail.com>
2023-10-25 02:14:13 +0700

    adjust home button

--- components/Kcomponent/Returnhome.tsx
+++ components/Kcomponent/Returnhome.tsx
@@ -3,27 +3,26 @@ import React from "react";
 
 const GoHome = () => {
   return (
-    <div>
+    <div className="flex justify-end ...">
       <Link href="/homepage">
-        <button
-          className="btn btn-outline btn-error mx-7 "
-          onClick={() => console.log("Click")}
-        >
-          Home
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="3"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"            />
-          </svg>
-          
+        <button className="mx-4 my-4 block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700">
+          <div className="flex items-center"> {/* Use flex to align elements horizontally */}
+            <span>Home</span> {/* Wrap "Home" in a span */}
+            <svg
+              //xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="3" /* Use strokeWidth instead of stroke-width */
+              stroke="currentColor"
+              className="h-6 w-6 ml-2" /* Add margin to separate "Home" and icon */
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </div>
         </button>
       </Link>
     </div>

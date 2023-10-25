0f8e6421dffa51a52e51ee4a6eccb050f20aa5c6
BBenneddyy <kanazaa2@gmail.com>
2023-10-26 02:49:27 +0700

    make get and post command for user model
    but can only try
     {const data = await prisma.user.create(products);
    
    (other element will error)

---
+++ app/api/getusers/route.ts
@@ -0,0 +1,56 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const data = await prisma.user.findMany({
+    orderBy: [
+      {
+        id: "desc",
+      },
+    ],
+  });
+  return NextResponse.json(data);
+}
+
+export async function POST(request: NextRequest) {
+  //request - object'
+  const req = await request.json();
+  //request to json
+  console.log(req);
+
+  const email = req.email
+  const name = req.name  
+  const lastname = req.lastname
+  const products = req.products
+  const remainbulb =req.remainbulb
+
+try {
+    const data = await prisma.user.create(products);
+
+//pls Help error around this section
+/*  try {
+    const data = await prisma.user.create({
+      data: {
+        name,
+        lastname,
+        products,
+        remainbulb : {create : remainbulb},
+        seller: {
+          connect: {
+            id: 1
+          }
+        }
+      },
+    });
+*/
+    console.log(data);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log("error" + error);
+    return NextResponse.json('error' + error)
+  }
+  
+}

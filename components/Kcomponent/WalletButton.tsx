a3edeae58963b6268c1dc916502500d513e992cb
BBenneddyy <kanazaa2@gmail.com>
2023-10-26 22:29:32 +0700

    button component

---
+++ components/Kcomponent/WalletButton.tsx
@@ -0,0 +1,18 @@
+"use clients"
+import { transform } from "next/dist/build/swc";
+import Link from "next/link";
+import React from "react";
+import './styles.css' 
+
+const Walletbutt = () => {
+    return(
+<div className="Censor">
+  <span>Bulb:999999</span>
+  <span className="top"></span>
+  <span className="bottom"></span>
+  <span className="left"></span>
+  <span className="right"></span>
+</div>)
+}
+
+export default Walletbutt

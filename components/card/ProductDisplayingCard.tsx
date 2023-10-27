4db39cd95730817d537cbbfcac7f545aeb0aa17a
ittipat <39715559+ittipatken@users.noreply.github.com>
2023-10-28 06:12:43 +0700

    add edit modal (#7)

--- components/card/ProductDisplayingCard.tsx
+++ components/card/ProductDisplayingCard.tsx
@@ -1,12 +1,16 @@
+const handleClick = () => {
+  // buy product
+}
+
 export default function ProductDisplayingCard(props: any) {
   return (
     <>
-      <div className="card w-80 bg-neutral text-neutral-content flex-auto m-1">
+      <div className="card w-80 bg-base-300 shadow-xl flex-auto m-1">
         <div className="card-body">
           <h2 className="card-title">{props.name}</h2>
           <p>{props.description}</p>
           <div className="card-actions">
-            <button className="btn btn-primary">Price: {props.price}</button>
+            <button className="btn btn-primary" onClick={handleClick}>Price: {props.price}</button>
           </div>
         </div>
       </div>

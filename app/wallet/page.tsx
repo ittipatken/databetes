c9973e283c92388a4bfd4cbc75a76c9e6fe11106
Akukinaqua <guynachanok2@gmail.com>
2023-10-28 22:41:10 +0700

    wallet(basic)

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,8 +1,52 @@
-export default function Wallet() {
+"use client";
+
+import { useEffect, useState } from "react";
+import WalletBox from "@/components/Wallet/WalletBox";
+import { useSession } from "next-auth/react";
+
+type AccountType = {
+  id: number;
+  name: string;
+  email: string;
+  lastname: string;
+  amount: number;
+};
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    const [accounts, setAccounts] = useState<AccountType>();
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch("/api/account", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          const data = await response.json();
+          setAccounts(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchData();
+    }, []);
     return (
-        <>
-            <h1>Pay_history</h1>
-            <p>transfer to B: 300 bulb</p>
-        </>
-    )
+      <>
+        <div className="flex flex-wrap m-4">
+          <WalletBox name={accounts?.name} amount={accounts?.amount} />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>please log in</h1>
+      </>
+    );
+  }
 }

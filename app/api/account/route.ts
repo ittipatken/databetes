4827f1653fd5f1d86f1856b570911f85c2ccb751
ittipat <39715559+ittipatken@users.noreply.github.com>
2023-10-29 20:28:45 +0700

    remove unused constants

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -40,13 +40,5 @@ export async function POST(request: NextRequest) {
     }
   });
 
-  const credential = await prisma.userCredential.create({
-    data: {
-      userid: data.id,
-      chula: chulaId,
-      password
-    }
-  })
-
   return NextResponse.json(data);
 }

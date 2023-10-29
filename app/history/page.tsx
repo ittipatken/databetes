d3cc87822985a7d53556482e20ecb5bc7db66ea2
BBenneddyy <kanazaa2@gmail.com>
2023-10-29 11:21:34 +0700

    draft payhistory 2#

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,25 +1,26 @@
 "use client";
 
-import BuyHistoryBox from "@/components/History/BuyHistoryBox";
+import Paidhistory from "@/components/History/Paidhistory";
 import { useEffect, useState } from "react";
-import { useSession } from 'next-auth/react';
+import { useSession } from "next-auth/react";
 import { Session } from "inspector";
-type ProductType = {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-  quantity: number;
-  seller: number
+import Pleaselogin from "@/components/Home/Pleaselogin";
+type TransactiondataType = {
+  amount: number ;
+  recieverEmail: string ;
+
+  //help here 1#
 };
 
 export default function History() {
-  const [products, setProducts] = useState<ProductType[]>([]);
+  const [transactiondata, setTransactiondata] = useState<TransactiondataType[]>(
+    []
+  );
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/products", {
+        const response = await fetch("/api/transaction", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -27,101 +28,60 @@ export default function History() {
         });
 
         const data = await response.json();
-        setProducts(data);
+        setTransactiondata(data);
       } catch (error) {
         console.error(error);
       }
     };
     fetchData();
-    
   }, []);
 
-    const { data: session? } = useSession();
-   let currentuser = session?.map(session=>session.id)
-    if(session){
-        return(
-            <>
-            <h1> you are logged in as {JSON.stringify(session)}</h1>
-            </>
-        )
-    }
-    else return(
-        <>
-        
-        </>
-    )
+  const { data: session } = useSession();
+  let currentuserid = session?.user.id
+  const userrecieve = transactiondata.filter(transactiondata.receiverid = currentuserid)
+  const userpaid = transactiondata.filter(transactiondata.senderid = currentuserid)
+
+  if (session) {
+    return (
+      <>
+        <div className="mt-4 md:mt-8">
+        <a
+          className="inline-block rounded bg-red-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-red-800 focus:outline-none focus:ring focus:ring-yellow-400 "
+        >
+          Paid history
+        </a>
+        </div>
+
+        <div className="flex flex-wrap gap-4 m-3">
+          {userpaid.map((userpaid) => (
+            <div key={test.id}>
+              <Paidhistory
+                name={userpaid.name}
+                price={userpaid.amount}
+              />
+            </div>
+          ))}
+
+
+        </div>
+
+        <div className="mt-4 md:mt-8">
+        <a
+          className="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
+        >
+          Recieve history
+        </a>
+        </div>
+        <div>
+
+          
+        </div>
+
+      </>
+    );
+  } else return <>
+  <Pleaselogin/>
+  </>;
+
+
 }
-
-  const test  = [{ id: 1, name: "www", price: 999, description: "asdkmsfsmdpfsp" }, { id: 1, name: "ppp", price: 333, description: "hhhhsdgftiiaisdiasiiacioinocnioscni" }];
-
-  
-    <>
-      <div className="flex flex-wrap gap-4 m-3">
-        {test.map((test) => (
-          <div key={test.id}>
-            <BuyHistoryBox
-              name={test.name}
-              price={test.price}
-              description={test.description} />
-          </div>))}
-          [Session]
-      </div>
-    </>
-
-
-
-  /*
-type userdatatype = {
-  id  : number
-  email : string
-  name : string     
-  lastname : String
-  products: ProductType[]
-} 
-
-  const [userinfo, setUserinfo] = useState<userdatatype[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/getusers", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-
-        const data = await response.json();
-        setUserinfo(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
-*/
-  /*
-  let arrayofpay : number[] = products.map(p=>p.price)
-  
-  //et arrayofproduct : Array[]= userinfo.map(p=>p.products)
-
-  //let userproduct= userinfo.products
-
-  let sumpay = arrayofpay.reduce((sum, currentvalue) => sum + currentvalue, 0)
-  */
-
-//let test = [{id: 1, name: "www" , price: 999}, {id: 1, name: "ppp" , price : 333}]
-/*
-return (
-  <>
-  <div className="flex flex-wrap">
-      {test.map((test) => (
-        <div key={test.id}>
-          <BuyHistoryBox
-            name={test.name}
-            price={test.price}/>
-        </div>))}
-  </div>
-    </>
-)}
-      */

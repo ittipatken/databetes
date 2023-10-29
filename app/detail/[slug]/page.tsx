8b82f5785bce97453b5598d93377af955c1cb432
ittipat <39715559+ittipatken@users.noreply.github.com>
2023-10-29 08:54:17 +0700

    add UI to detail page

--- app/detail/[slug]/page.tsx
+++ app/detail/[slug]/page.tsx
@@ -4,5 +4,16 @@ export default function DeatilSlug({ params }: { params: { slug: string } }) {
         return decodedString;
     }
     const decodedString = urldecode();
-    return <div>My Post: {decodedString}</div>;
+    return (
+        <div className="flex justify-center">
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body items-center text-center">
+                    <p>{decodedString}</p>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">ซื้อเลย</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }

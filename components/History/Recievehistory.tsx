4bf4ba3cf237a008cb4f7e2166e354c8f1b8ae5c
BBenneddyy <kanazaa2@gmail.com>
2023-10-29 11:52:20 +0700

    adjust text display and color

--- components/History/Recievehistory.tsx
+++ components/History/Recievehistory.tsx
@@ -1,11 +1,11 @@
 export default function Paidhistory(props: any) {
     return (
-        <div className="card w-96 bg-green-100 border-solid border-8 border-green-600">
+        <div className="card w-96 bg-green-200 border-solid border-8 border-green-600">
           <div className="card-body">
-            <h2 className="font-bold text-xl underline underline-offset-4 decoration-4">{props.name}</h2>
+            <h2 className="font-bold text-xl underline underline-offset-4 decoration-4">จาก {props.name}</h2>
             
             <div className="card-actions justify-end">
-              <button className="btn bg-black text-white"> + {props.price} bulb
+              <button className="btn text-black bg-white"> + {props.price} bulb
               </button>
             </div>
           </div>

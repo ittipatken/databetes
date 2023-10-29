8801451628ac74cbf23fa2ab1671e0512f310e5f
Punyawish Patumhirunruksa <87409329+Punpunyawish@users.noreply.github.com>
2023-10-29 16:07:15 +0700

    add add user API

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -22,3 +22,31 @@ export async function GET() {
   return NextResponse.json(data);
 }
 
+export async function POST(request: NextRequest) {
+
+  const req = await request.json()
+  const email = req.email? req.email: 'noemail@error.com';
+  const name = req.name? req.name: 'no_name';
+  const lastname = req.lastname? req.lastname: 'no_lastname';
+  const chulaId = req.chulaId? req.chulaId: 0;
+  const password = req.password? req.password: 0;
+
+  const data = await prisma.user.create({
+    data: {
+      email,
+      name,
+      lastname,
+      amount: 2000,
+    }
+  });
+
+  const credential = await prisma.userCredential.create({
+    data: {
+      userid: data.id,
+      chula: chulaId,
+      password
+    }
+  })
+
+  return NextResponse.json(data);
+}

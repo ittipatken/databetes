7ac2a2357de592c815e953bc6f67d58970739a81
ittipat <39715559+ittipatken@users.noreply.github.com>
2023-10-29 21:08:55 +0700

    update text for product with no detail

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -2,7 +2,7 @@ export default function Detail() {
   return (
     <div className="card w-screen bg-red-300 border-black m-5 max-w-4xl mx-auto">
       <div className="card-body">
-        <h2 className="text-center">ไม่พบสินค้า</h2>
+        <h2 className="text-center">สินค้านี้ไม่มีรายละเอียดเพิ่มเติม</h2>
       </div>
     </div>
   );

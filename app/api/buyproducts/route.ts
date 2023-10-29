fa227a31fcbf670826323bf22967d3ffecdde224
Punyawish Patumhirunruksa <87409329+Punpunyawish@users.noreply.github.com>
2023-10-29 18:25:47 +0700

    update buying product api

--- app/api/buyproducts/route.ts
+++ app/api/buyproducts/route.ts
@@ -1,2 +1,105 @@
 //ส่งมาว่าซื้ออะไร กี่ชิ้น เป็นเงินกี่ bulb
 //{productId: 1, quantity: 2, amount: 30}
+
+import { type NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth/next";
+import { config } from "@/app/lib/auth";
+
+const prisma = new PrismaClient();
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const data = await prisma.product.findMany({
+    orderBy: [
+      {
+        id: "desc",
+      },
+    ],
+  });
+  return NextResponse.json(data);
+}
+
+export async function POST(request: NextRequest) {
+  //request - object'
+  const req = await request.json();
+  const session = await getServerSession(config);
+
+  const name = req.name;
+  const price = req.price;
+  const quantity = req.quantity
+  const description = req.description
+
+  if(!session || !session.user?.email){
+    return NextResponse.json({status: 'error'})
+  }
+
+  const getUser = await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    }
+  })
+  
+  const userId = Number(getUser?.id)
+
+  try {
+    const data = await prisma.product.create({
+      data: {
+        name,
+        price,
+        quantity,
+        description,
+        userId,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json('error' + error)
+  }
+  
+}
+
+export async function PUT(request: NextRequest) {
+  //request - object'
+  const req = await request.json();
+  const session = await getServerSession(config);
+
+  const id = req.id;
+  const name = req.name;
+  const price = req.price;
+  const quantity = req.quantity
+  const description = req.description
+
+  if(!session || !session.user?.email){
+    return NextResponse.json({status: 'error'})
+  }
+
+  const getUser = await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    }
+  })
+  
+  const userId = Number(getUser?.id)
+
+  try {
+    const data = await prisma.product.update({
+      where: {
+        id
+      },
+      data: {
+        name,
+        price,
+        quantity,
+        description,
+        userId,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json('error' + error)
+  }
+  
+}

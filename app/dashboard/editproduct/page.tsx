d358359ba0e791269cd9f07d4d222959603688b3
ittipat <39715559+ittipatken@users.noreply.github.com>
2023-10-30 17:37:53 +0700

    fix rendering error if products is empty

--- app/dashboard/editproduct/page.tsx
+++ app/dashboard/editproduct/page.tsx
@@ -36,17 +36,18 @@ export default function EditProduct() {
     <div className="m-w-screen-md xl:w-3/4 mx-auto mt-4">
       <h1 className="ml-8 font-bold text-5xl">แก้ไขรายการสินค้า</h1>
       <div className="flex flex-wrap gap-2 m-4">
-        {products.map((product) => (
-          <div key={product.id}>
-            <EditProductCard
-              id={product.id}
-              name={product.name}
-              description={product.description}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          </div>
-        ))}
+        {products.length !== 0 &&
+          products.map((product) => (
+            <div key={product.id}>
+              <EditProductCard
+                id={product.id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                quantity={product.quantity}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
